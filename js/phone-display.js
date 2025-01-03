4a2f7280bb5e763b924f136704af4a2fec23e788
Rasyid Nabil P <rasyidnabil06@gmail.com>
2025-01-03 17:23:19 +0700

    add css styles for mobile

--- js/phone-display.js
+++ js/phone-display.js
@@ -1,12 +1,1 @@
-// Functions to open SIdebar
-
-const sidebar = document.getElementById("sidebar");
-const menuBurger = document.getElementById("menu-burger");
-
-menuBurger.addEventListener("click", () => {
-   if (sidebar.style.left === "0px") {
-      sidebar.style.left = "-300px";
-   } else {
-      sidebar.style.left = "0px";
-   }
-});
+//  This isi for Javascript
